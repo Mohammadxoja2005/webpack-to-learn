f659ffafd0efb7a6ecc782a3c98899ebeee3d7e4
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2022-12-27 00:07:46 +0500

    adding first webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import joker from "./joke"; 
+import './styles/main.scss'; 
+// import img from "./assets/IMG_B.jpg"; 
+
+const imgId = document.querySelector("#hello"); 
+// imgId.src = img; 
+
+console.log(joker()); 
---
+++ src/joke.js
@@ -0,0 +1,5 @@
+function joker() {
+    return 'this is joke haahhaha so funny';    
+} 
+
+export default joker; 
